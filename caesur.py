ff05fcd3aa028a29e1226d669021c4a37036c44d
Shamimgardobuya <shamimobuya@gmail.com>
2022-05-17 19:55:22 +0300

    looping through

---
+++ caesur.py
@@ -0,0 +1,15 @@
+# import string
+# text="Hello World"
+# def secure(text,shift,string_upper):
+def divisors(integer):
+     for i in range(1,integer+1):
+          if i%integer ==0:
+               print(list(i))
+          else:
+            print(f"{integer}is prime") 
+divisors(12)
+for x in range(1,7):
+      p=[]
+      print(p.append(x))
+     
+      
